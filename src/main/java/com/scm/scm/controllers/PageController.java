18c5916c890f53fc05885cb8aebb4a163111a13b
shivainnovatrix <shiva@innovatrixinfotech.com>
2024-05-03 19:55:22 +0530

    first commit

---
+++ src/main/java/com/scm/scm/controllers/PageController.java
@@ -0,0 +1,30 @@
+package com.scm.scm.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    
+    public String home(Model model){
+        System.out.println("home page handler");   // sending data to view (Dynamic data send )
+        model.addAttribute("name", "Aman verma");
+        return "home";
+    }
+
+    //about
+    @RequestMapping("/about")
+    public String aboutPage(){
+        System.out.println("about page loading...");
+        return "about";
+    }
+
+
+    // Services
+}
